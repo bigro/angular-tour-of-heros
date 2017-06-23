fac31716ba8a930e5279924b0777d641f0f9808f
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-06-23 21:03:20 +0900

    tutorial

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+
+import {HeroesComponent} from "./heroes.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {AppComponent} from "./app.component";
+import {HeroService} from "./hero.service";
+import {DashboardComponent} from "./dashboard.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import { InMemoryDataService }  from './in-memory-data.service';
+import {HeroSearchComponent} from "./hero-search.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    HeroSearchComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
+})
+export class AppModule {
+}
